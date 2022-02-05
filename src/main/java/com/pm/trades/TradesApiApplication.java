303ef49258526d16bc031f35c40f0c49d6db3f23
Parth Makadiya <makadiyaparth98@gmail.com>
2022-02-05 09:35:46 +0530

    Initial commit

---
+++ src/main/java/com/pm/trades/TradesApiApplication.java
@@ -0,0 +1,13 @@
+package com.pm.trades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/trades/TradesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.trades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
