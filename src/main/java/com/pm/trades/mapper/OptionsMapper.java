3ad3b99604419cccae2a989375ab620308e70539
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 05:25:37 +0530

    add options mapper

---
+++ src/main/java/com/pm/trades/mapper/OptionsMapper.java
@@ -0,0 +1,22 @@
+package com.pm.trades.mapper;
+
+import com.pm.trades.dto.OptionsDTO;
+import com.pm.trades.dto.OptionsInDTO;
+import com.pm.trades.entity.OptionsEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring", uses = InstrumentMapper.class)
+public interface OptionsMapper {
+    @Mapping(target = "instrumentDTO", source = "instrumentEntity")
+    OptionsDTO toDTO(OptionsEntity optionsEntity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "instrumentEntity", ignore = true)
+    OptionsEntity toEntity(OptionsInDTO optionsInDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "instrumentEntity", ignore = true)
+    void updateEntity(@MappingTarget OptionsEntity optionsEntity, OptionsInDTO optionsInDTO);
+}
