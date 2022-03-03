92504b389434ff8f891edfc4b25b5c903875b287
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 05:19:22 +0530

    add jpa method to search instrument by name

--- src/main/java/com/pm/trades/repository/InstrumentRepository.java
+++ src/main/java/com/pm/trades/repository/InstrumentRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InstrumentRepository extends JpaRepository<InstrumentEntity, Integer> {
     List<InstrumentEntity> findByNameStartingWith(String name);
+
+    Optional<InstrumentEntity> findByName(String name);
 }
